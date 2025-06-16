385efeeb4697262ccef68d5d0f626040b0a1ab7e
AraMinjibir <abubakar.aramjb@gmail.com>
2025-06-16 17:29:36 +0100

    Event Model

---
+++ src/main/resources/event-booking/src/app/Model/event.ts
@@ -0,0 +1,11 @@
+export class Event {
+
+    id: number;
+    location: string;
+    dateTime: string;       
+    halls: string;
+    totalSeat: number;
+    availableSeat: number;
+    description: string;
+    imageUrl: string;
+}
