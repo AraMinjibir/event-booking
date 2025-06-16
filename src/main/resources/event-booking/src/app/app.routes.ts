6dd4e587c34835886a2650262aa45c9aa6186afa
AraMinjibir <abubakar.aramjb@gmail.com>
2025-06-16 17:33:50 +0100

    Admin route

--- src/main/resources/event-booking/src/app/app.routes.ts
+++ src/main/resources/event-booking/src/app/app.routes.ts
@@ -2,22 +2,28 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './Auth/signup/signup.component';
 import { LoginComponent } from './Auth/login/login.component';
 import { PageNotFoundComponent } from './Auth/page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
-    path: "signup",
-    component: SignupComponent},
-    {
-      path: "login",
-      component: LoginComponent},
-  {
-    redirectTo: 'login',
-    path: '',
-    pathMatch:'full'
-    
+    path: "home",
+    component: HomeComponent
   },
 {
+  redirectTo: 'home',
+  path: '',
+  pathMatch:'full'
+  
+},
+  {
+    path: "auth/signup",
+    component: SignupComponent},
+    {
+      path: "auth/login",
+      component: LoginComponent},
+    
+{
   path: "admin",
-  loadChildren: () => import("./admin/admin.module").then(a => a.AdminModule)
+  loadChildren: () => import("./admin-layout/admin.module").then(a => a.AdminModule)
 },
 { path: '**', component: PageNotFoundComponent }];
