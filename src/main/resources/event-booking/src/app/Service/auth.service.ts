0b6f41a369854afc53b6933b9f0863c1b0441257
AraMinjibir <abubakar.aramjb@gmail.com>
2025-06-16 17:26:58 +0100

    backend API added

--- src/main/resources/event-booking/src/app/Service/auth.service.ts
+++ src/main/resources/event-booking/src/app/Service/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Auth } from '../Model/auth';
+import { Booking } from '../Model/booking';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { Auth } from '../Model/auth';
 export class AuthService {
   private baseUrl = 'http://localhost:8080/api/auth/register';
   private baseUrl2 = 'http://localhost:8080/api/auth/login';
+  private bookUrl = 'http://localhost:8080/api/books/getBooks'
 
   constructor(private http: HttpClient) { }
 
@@ -18,5 +20,8 @@ export class AuthService {
 
   login(data: {email:string; password:string}):Observable<Auth>{
     return this.http.post<Auth>(this.baseUrl2, data);
+  }
+  getBook():Observable<Booking[]>{
+    return this.http.get<Booking[]>(this.bookUrl);
   }
 }
