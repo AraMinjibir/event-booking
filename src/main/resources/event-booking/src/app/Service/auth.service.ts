6dcc835634fe2c38d121b84a5f954d7a50bab151
AraMinjibir <abubakar.aramjb@gmail.com>
2025-06-17 20:58:59 +0100

    logout functionality & authguard

--- src/main/resources/event-booking/src/app/Service/auth.service.ts
+++ src/main/resources/event-booking/src/app/Service/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Auth } from '../Model/auth';
 import { Booking } from '../Model/booking';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class AuthService {
   private baseUrl2 = 'http://localhost:8080/api/auth/login';
   
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   createUser(auth: Auth):Observable<Auth>{
     return this.http.post<Auth>(this.baseUrl, auth);
@@ -21,5 +22,12 @@ export class AuthService {
   login(data: {email:string; password:string}):Observable<Auth>{
     return this.http.post<Auth>(this.baseUrl2, data);
   }
-  
+  logout(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['auth/login']); 
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
 }
